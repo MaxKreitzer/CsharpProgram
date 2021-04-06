05945fdc180b49bf08e646e285e90aca04916671
MaxKreitzer <201699@pennmanor.net>
2021-04-06 08:22:18 -0400

    Number Guessing Game + Asking For Name

--- Program.cs
+++ Program.cs
@@ -6,11 +6,29 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+
+            Console.WriteLine("Guess the from 1 to 10.");
+            int num = random.Next(0,11);
+            int inp = Convert.ToInt32(Console.ReadLine());
+
+            while (inp != num)
+            {
+                if (inp < num)
+                {
+                    Console.WriteLine("Too low");
+                    inp = Convert.ToInt32(Console.ReadLine());
+                }
+                if (inp > num)
+                {
+                    Console.WriteLine("Too High");
+                    inp = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Correct!");
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
-            
-            Console.WriteLine("Hello world");
         }
     }
 }
